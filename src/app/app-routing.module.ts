d0a04bbe4767667c134aa60f53595c8c8340b13a
ledys09 <alvaradoestefany51@yahoo.es>
2020-10-10 19:36:10 -0600

    add dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { LoginComponent } from './pages/login/login.component';
-import { EnterprisesComponent } from './pages/enterprises/enterprises.component';
-import { AboutComponent } from './pages/about/about.component';
-import { RegisterClientComponent } from './pages/register/register-client.component';
-import { RegisterEnterpriseComponent } from './pages/register/register-enterprise.component';
+
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { EnterprisesComponent } from './enterprises/enterprises.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterClientComponent } from './register/client/register-client.component';
+import { RegisterEnterpriseComponent } from './register/enterprise/register-enterprise.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FilesComponent } from './pages/files/files.component';
+import { PagenofoundComponent } from './shared/pagenofound/pagenofound.component';
+
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'register', component: RegisterComponent,
-          children: [
-            { path: 'enterprise', component: RegisterEnterpriseComponent },
-            { path: 'client', component: RegisterClientComponent },
-            { path: '**', pathMatch: 'full', redirectTo: 'home' },
-          ] },
+
+  { path: '', component: HomeComponent },
+  { path: 'register/enterprise', component: RegisterEnterpriseComponent },
+  { path: 'register/client', component: RegisterClientComponent },
   { path: 'login', component: LoginComponent },
   { path: 'enterprises', component: EnterprisesComponent },
   { path: 'about', component: AboutComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home'},
-  { path: ' ', pathMatch: 'full', redirectTo: 'home'}
+  { path: 'home', component: PagesComponent,
+    children: [
+      { path: 'progress', component: ProgressComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'files', component: FilesComponent },
+      { path: '**', pathMatch: 'full', redirectTo: 'dashboard'},
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard'}
+    ]},
+  { path: '**', component: PagenofoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NavbarComponent } from './pages/shared/navbar/navbar.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { EnterprisesComponent } from './pages/enterprises/enterprises.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { EnterprisesComponent } from './enterprises/enterprises.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AboutComponent } from './pages/about/about.component';
-import { RegisterClientComponent } from './pages/register/register-client.component';
-import { RegisterEnterpriseComponent } from './pages/register/register-enterprise.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterClientComponent } from './register/client/register-client.component';
+import { RegisterEnterpriseComponent } from './register/enterprise/register-enterprise.component';
 import { NavColorDirective } from './directives/nav-color.directive';
-import { FooterComponent } from './pages/shared/footer/footer.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PagenofoundComponent } from './shared/pagenofound/pagenofound.component';
+import { DashboardComponent} from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { FilesComponent } from './pages/files/files.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './pages/pages.component';
 
 
 @NgModule({
@@ -28,7 +36,15 @@ import { FooterComponent } from './pages/shared/footer/footer.component';
     RegisterClientComponent,
     RegisterEnterpriseComponent,
     NavColorDirective,
-    FooterComponent
+    FooterComponent,
+    PagenofoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    FilesComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
